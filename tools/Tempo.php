c257d9fba8460208bec4c5998d11c29e04d57df0
Protocol Corporation <protocollive@outlook.com>
2026-03-27 17:05:08 -0300

    Cabeçalho e correção das cinzas

--- tools/Tempo.php
+++ tools/Tempo.php
@@ -1,11 +1,16 @@
 <?php
+//Protocol Corporation Ltda.
+//https://github.com/ProtocolLive/ApiCatolica
 
+/**
+ * @version 2026.03.27.00
+ */
 enum Tempo:string{
   case Advento = 'adv';
   case Natal = 'ntl';
   case Epifania = 'epf';
   case Comum = 'tc';
-  case Cinzas = 'cz';
+  case Cinzas = 'czs';
   case Quaresma = 'qrm';
   case Ramos = 'rms';
   case Pascoa = 'psc';
